0e344f9b66fb0b90634b451c689ad9255238d40b
Mikael <mikael@rubixy.com>
2020-01-30 20:46:32 +0100

    Remove the UnknownCommand : Command Spam

--- Services/CommandHandlingService.cs
+++ Services/CommandHandlingService.cs
@@ -27,7 +27,17 @@ namespace TastyBot.Services
 
             //Hook Messages so we can process them if they're commands.
             _discord.MessageReceived += OnMessageReceivedAsync;
+            _commands.CommandExecuted += OnCommandExecuted;
 
+        }
+
+        private async Task OnCommandExecuted(Optional<CommandInfo> command, ICommandContext context, IResult result)
+        {
+            if (!command.IsSpecified) //if the command does not exist, truncate response
+                return;
+
+            if (!result.IsSuccess) // If not successful, reply with the error.
+                await context.Channel.SendMessageAsync(result.ToString());
         }
 
         private async Task OnMessageReceivedAsync(SocketMessage s)
@@ -38,18 +48,11 @@ namespace TastyBot.Services
 
             var context = new SocketCommandContext(_discord, msg);      // Create the command context
 
-
-
-
             int argPos = 0;                                             // Check if the message has a valid command prefix
             if (msg.HasStringPrefix(_config["prefix"], ref argPos) || msg.HasMentionPrefix(_discord.CurrentUser, ref argPos))
             {
+
                 var result = await _commands.ExecuteAsync(context, argPos, _provider);      // Execute the command
-
-                if (!result.IsSuccess)                                                      // If not successful, reply with the error.
-                    await context.Channel.SendMessageAsync(result.ToString());
-
-
             }
         }
     }
