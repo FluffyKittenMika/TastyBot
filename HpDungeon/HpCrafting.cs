9c6e1c47308c3c89a2491f9c4ea7ce764f493f24
Mikael <mikael@rubixy.com>
2020-02-07 18:43:47 +0100

    Crafting Class update

--- HpDungeon/HpCrafting.cs
+++ HpDungeon/HpCrafting.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace TastyBot.HpDungeon
@@ -24,19 +25,46 @@ namespace TastyBot.HpDungeon
 
     class HpCrafting
     {
-
-
         /// <summary>
         /// Makes an item
         /// </summary>  
         /// <param name="target">What we want to make</param>
         /// <param name="inventory">Inventory we'll query items from</param>
+        /// <returns></returns>
         public List<HpItem> Craft(Recepie target, List<HpItem> inventory)
         {
+            bool canCraftItem = false;
+          
             foreach (var needs in target.Requirements)
             {
-                //work in progesss
-                inventory = new List<HpItem>();
+                try
+                {
+                    //Count the inventory, for each item, that has the needs object in it as a key. 
+                    //That gives us the total amount of X item in Inventory of the recepie, and they all have to match up
+                    int count = inventory.Count(x => x.Equals(needs.Key));
+                    if (count >= needs.Value) // check if the player has enough of said item
+                        canCraftItem = true;
+                }
+                catch (Exception)
+                {
+                    //can't make the item if it fails.
+                    return inventory;
+                }
+            }
+
+
+            //if it can craft it after checking all the stuff, it's time we remove that from the player, and give the result
+            if (canCraftItem)
+            {
+                foreach (var substractableitem in target.Requirements)
+                {
+                    for (int i = 0; i < substractableitem.Value; i++) //we have X amount of those that needs to be removed
+                    {
+                        inventory.Remove(substractableitem.Key); //remove one of the item
+                    }
+                }
+                //add the result
+                inventory.Add(target.Result);
             }
             return inventory;
         }
