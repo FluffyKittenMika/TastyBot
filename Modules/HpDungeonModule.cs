a8dc966cfe2740809a4cc16106fe3833a7d6faa7
Mikael <mikael@rubixy.com>
2020-02-12 20:03:12 +0100

    Ram storage, saves every CONFIG command.

--- Modules/HpDungeonModule.cs
+++ Modules/HpDungeonModule.cs
@@ -23,6 +23,12 @@ namespace TastyBot.HpDungeon
 		private readonly FileManager fileManager;
 		private readonly Random _random;
 		private readonly HpCrafting crafter;
+
+		//Static retains memory. 
+		private static Dictionary<string,HpPlayer> PlayerRamMemory = new Dictionary<string, HpPlayer>();
+		private static int savecounter = 0;
+
+
 		/// <summary>
 		/// Constructor, this one is called automagically through reflection
 		/// </summary>
@@ -37,6 +43,7 @@ namespace TastyBot.HpDungeon
 			fileManager.Init();
 			crafter = new HpCrafting();
 			Container.LoadItems();
+
 		}
 
 		/// <summary>
@@ -45,40 +52,105 @@ namespace TastyBot.HpDungeon
 		/// <returns>A new player, or auto loads a player</returns>
 		private async Task<HpPlayer> GetPlayer(IUser user)
 		{
+			//TODO: Find a better way that uses time or something. Every 5'th message is kinda not that good
+			savecounter++;
+			if (savecounter > Convert.ToInt32(_config["savecounter"]))
+			{
+				SaveAllPlayers();
+				savecounter = 0;
+			}
 			HpPlayer p = null;
 
-			try
+			if (PlayerRamMemory.ContainsKey(user.Id.ToString())) //if it already is loaded
 			{
-				p = (await fileManager.LoadData<HpPlayer>(user.Id.ToString())).FirstOrDefault();
+				p = PlayerRamMemory[user.Id.ToString()] ;
 			}
-			catch (Exception e)
+			else //fetch from slow storage
 			{
-				Console.WriteLine(e.Message + " Earan fucked up somewhere :)"); //this means Earan fucked up
-			}
-
-			//if it's null, we make a new one, and init it
-			if (p == null)
-			{
-				Console.WriteLine("New player: " + user.Id.ToString());
-				p = new HpPlayer(user.Id.ToString())
+				try
 				{
-					ID = user.Id.ToString(),
-					Skills = new Dictionary<string, int>()
-				};
-				await SavePlayer(p); //make base save aswell.
+					p = (await fileManager.LoadData<HpPlayer>(user.Id.ToString())).FirstOrDefault();
+				}
+				catch (Exception e)
+				{
+					Console.WriteLine(e.Message + " Earan fucked up somewhere :)"); //this means Earan fucked up
+				}
+
+				//if it's null, we make a new one, and init it
+				if (p == null)
+				{
+					Console.WriteLine("New player: " + user.Id.ToString());
+					p = new HpPlayer(user.Id.ToString())
+					{
+						ID = user.Id.ToString(),
+						Skills = new Dictionary<string, int>()
+					};
+					SavePlayerDisk(p); //make base save aswell.
+				}
+				//add to ram
+				PlayerRamMemory.Add(user.Id.ToString(), p);
 			}
+		
 			return p;
 		}
 
-		private async Task SavePlayer(HpPlayer player)
+
+
+		[Command("hpdsave")]
+		private void HpdSavePlayers()
+		{
+			if (Context.User.Id == 83183880869253120)
+			{
+				SaveAllPlayers();
+				savecounter = 0;
+			}
+			else
+				ReplyAsync("ask mik to run it. as this is global save.");
+		}
+
+
+		//TODO Move this to a different class
+		/// <summary>
+		/// Stores the player in the ram module
+		/// </summary>
+		/// <param name="player">Target player to store</param>
+		/// <returns>Fuck all</returns>
+		private void SavePlayer(HpPlayer player)
+		{
+			if (PlayerRamMemory.ContainsKey(player.ID))
+				PlayerRamMemory[player.ID] = player;
+			else
+				PlayerRamMemory.Add(player.ID, player);
+		}
+
+		/// <summary>
+		/// Saves all players from ram to Disk
+		/// </summary>
+		/// <returns>Fuck all</returns>
+		private void SaveAllPlayers()
+		{
+			foreach (var p in PlayerRamMemory)
+			{
+				SavePlayerDisk(p.Value);
+			}
+			Console.WriteLine("All players saved");
+		}
+
+		/// <summary>
+		/// Stores a single player to disk from ram
+		/// </summary>
+		/// <param name="player">Target player</param>
+		/// <returns>Nothing at all</returns>
+		private void SavePlayerDisk(HpPlayer player)
 		{
 			//yes i'm this lazy
 			List<HpPlayer> p = new List<HpPlayer>
 			{
 				player
 			};
-			await fileManager.SaveData(p, player.ID);
+			fileManager.SaveData(p, player.ID);
 		}
+
 
 		[Command("mine")]
 		[Summary("Go gather ores")]
@@ -118,7 +190,7 @@ namespace TastyBot.HpDungeon
 			if (currlvl < p.GetSkillLevel("mining"))					//Check if new current lvl is higher than old
 				levelup = true;                                         //If yes, then they've gained a lvl
 
-			await SavePlayer(p);                                        //Now we save the player
+			SavePlayer(p);												//Now we save the player to ram
 																		//And compile the resposne
 			string response = $"You've gone mining, and got an **{item.ItemName}** \r\n" +
 							  $"and gained {item.ItemXp}xp";
@@ -130,6 +202,7 @@ namespace TastyBot.HpDungeon
 
 		[Command("inventory")]
 		[Alias("inv")]
+		[Summary("Get your inventory")]
 		public async Task Inventory()
 		{
 			HpPlayer p = await GetPlayer(Context.User);
@@ -155,6 +228,7 @@ namespace TastyBot.HpDungeon
 		}
 
 		[Command("Craft")]
+		[Summary("Craft an object")]
 		public async Task Craft([Remainder]string craft)
 		{
 			//check if whatever the fuck they typed, is a key
@@ -179,7 +253,7 @@ namespace TastyBot.HpDungeon
 					response.Append($"You made a {craft} and gained {i.Result.ItemXp} {i.Skill} XP");
 					if (levelup)
 						response.Append($"\nYou gained a **{i.Skill} LEVEL!**");
-					await SavePlayer(p);
+					SavePlayer(p);
 					await ReplyAsync(response.ToString());
 				}
 				else if (!crafted && p.GetSkillLevel(i.Skill) <= i.Result.ItemLevel)
