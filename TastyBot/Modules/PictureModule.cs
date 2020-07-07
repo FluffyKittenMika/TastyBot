95ba8b27075d35c90194fe8397b66ce3fd3ac891
Mikael <Mikaelssenpro@gmail.com>
2020-07-07 12:46:41 +0200

    Supprort for action gifs, and nsfw gifs

--- TastyBot/Modules/PictureModule.cs
+++ TastyBot/Modules/PictureModule.cs
@@ -87,14 +87,26 @@ namespace TastyBot.Modules
 			await Context.Channel.SendFileAsync(s, "Fox.png");
 		}
 
+        [Command("waifu")]
+        public async Task WaifuAsync([Remainder] string text = "")
+        {
+            var s = await _nekoClientModule.SFWNekoClientPictureAsync(RegularNekos.Waifu, text);
+            await Context.Channel.SendFileAsync(s, "Waifu.png");
+        }
+
         #endregion
 
         #region NSFW Nekos owo
 
-        //TODO: ADD command descriptors to all commands in this module.
+        #region Images
+        /// <summary>
+        /// Gets a random NSFW image, if not otherwise specified in input
+        /// </summary>
+        /// <param name="text">enum name</param>
+        /// <returns>Lewd imagery directly to the discord</returns>
         [Command("NSFW")]
         [RequireNsfw]
-        public async Task NSFWAhegaoAsync([Remainder]string text = "")
+        public async Task NSFWAsync([Remainder]string text = "")
         {
             NSFWNekos res;
 
@@ -108,6 +120,11 @@ namespace TastyBot.Modules
             await Context.Channel.SendFileAsync(s, "OwO.png");
         }
 
+        /// <summary>
+        /// Attempts to get the relevant NSFW enum from a string
+        /// </summary>
+        /// <param name="nekoString">Input name</param>
+        /// <returns>NSFWNekos</returns>
         private NSFWNekos GetNSFWNekoFromString(string nekoString)
         {
             TextInfo textInfo = new CultureInfo("en-US", false).TextInfo;
@@ -119,15 +136,111 @@ namespace TastyBot.Modules
 
         #endregion
 
-        #region Action Nekos
-        [Command("Cuddle")]
-        public async Task CuddleAsync([Remainder] string text = "")
+        #region Gifs
+        /// <summary>
+        /// Gets an animated NSFW image, uses specified input text if any.
+        /// </summary>
+        /// <param name="text">Input text</param>
+        /// <returns>Lewd animations to your discord channel</returns>
+        [Command("NSFWGIF")]
+        [RequireNsfw]
+        public async Task NSFWGIFAhegaoAsync([Remainder] string text = "")
         {
-            var s = await _nekoClientModule.SFWNekoClientPictureAsync(RegularNekos.Neko, text);
-            await Context.Channel.SendFileAsync(s, "Cuffle.gif");
+            NSFWNekos res;
+
+            if (text == "")
+                res = RandomEnumValue<NSFWNekos>();
+            else
+                res = GetNSFWNekoFromString(text);
+
+            text = string.Join(' ', text.Split(' ').Skip(1));
+            var s = await _nekoClientModule.NSFWNekoClientPictureAsync(res, text);
+            await Context.Channel.SendFileAsync(s, "OwO.png");
+        }
+
+
+        /// <summary>
+        /// Attempts to get the relevant NSFW GIF enum from a string
+        /// </summary>
+        /// <param name="nekoString">Input name</param>
+        /// <returns>AnimatedNSFWNekos</returns>
+        private AnimatedNSFWNekos GetNSFWGifFromString(string nekoString)
+        {
+            TextInfo textInfo = new CultureInfo("en-US", false).TextInfo;
+            string NekoStringToParse = textInfo.ToTitleCase(nekoString.Split(' ').FirstOrDefault());
+            Console.WriteLine(NekoStringToParse);
+            Enum.TryParse(NekoStringToParse, out AnimatedNSFWNekos neko);
+            return neko;
         }
         #endregion
+        #endregion
 
+        #region Action Nekos
+        [Command("Cuddle")]
+        public async Task CuddleAsync()
+        {
+            var s = await _nekoClientModule.ActionNekoClientPictureAsync(ActionNekos.Cuddlegif);
+            await Context.Channel.SendFileAsync(s, "Cuffle.gif");
+        }
+
+        [Command("Feed")]
+        public async Task FeedAsync()
+        {
+            var s = await _nekoClientModule.ActionNekoClientPictureAsync(ActionNekos.Feedgif);
+            await Context.Channel.SendFileAsync(s, "Feed.gif");
+        }
+
+        [Command("Hug")]
+        public async Task HugAsync()
+        {
+            var s = await _nekoClientModule.ActionNekoClientPictureAsync(ActionNekos.Huggif);
+            await Context.Channel.SendFileAsync(s, "Hug.gif");
+        }
+
+        [Command("Kiss")]
+        public async Task KissAsync()
+        {
+            var s = await _nekoClientModule.ActionNekoClientPictureAsync(ActionNekos.Kissgif);
+            await Context.Channel.SendFileAsync(s, "Kiss.gif");
+        }
+
+        [Command("Pat")]
+        public async Task PatAsync()
+        {
+            var s = await _nekoClientModule.ActionNekoClientPictureAsync(ActionNekos.Patgif);
+            await Context.Channel.SendFileAsync(s, "Pat.gif");
+        }
+
+        [Command("Poke")]
+        public async Task PokeAsync()
+        {
+            var s = await _nekoClientModule.ActionNekoClientPictureAsync(ActionNekos.Pokegif);
+            await Context.Channel.SendFileAsync(s, "Poke.gif");
+        }
+
+        [Command("Slap")]
+        public async Task SlapAsync()
+        {
+            var s = await _nekoClientModule.ActionNekoClientPictureAsync(ActionNekos.Slapgif);
+            await Context.Channel.SendFileAsync(s, "Slap.gif");
+        }
+
+        [Command("Tickle")]
+        public async Task TickleAsync()
+        {
+            var s = await _nekoClientModule.ActionNekoClientPictureAsync(ActionNekos.Ticklegif);
+            await Context.Channel.SendFileAsync(s, "Tickle.gif");
+        }
+
+
+        #endregion
+
+
+        /// <summary>
+        /// Gets a random enum from a Enum Collection
+        /// </summary>
+        /// <typeparam name="T">Enum Type</typeparam>
+        /// <returns>Random Enum from specified collection</returns>
         private T RandomEnumValue<T>()
         {
             var v = Enum.GetValues(typeof(T));
